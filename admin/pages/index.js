6b7f573de4dee51f630f00528219ba635461bb71
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-08-25 12:09:38 -0300

    Admin authentication (next-auth, mongodb)

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,5 +1,33 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div className='bg-blue-900 w-screen h-screen flex items-center'>
+        <div className='text-center w-full'>
+          <button 
+            className='bg-white p-2 px-4 rounded-lg'
+            onClick={() => signIn('google')}
+          >
+            Login com Google
+          </button>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className={'bg-red-500'}>Hello World</div>
+    <div className='bg-blue-900 w-screen h-screen flex items-center'>
+      <div className='text-center w-full'>
+        <button 
+          className='bg-white p-2 px-4 rounded-lg'
+          onClick={() => signOut()}
+        >
+          Logado com {session.user.email}
+        </button>
+      </div>
+    </div>
   )
 }
