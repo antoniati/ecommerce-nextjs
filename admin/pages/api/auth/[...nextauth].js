e8c0536a3de79be8e8a9a1ff5f6ce7cd60fc8958
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-08-27 11:59:37 -0300

    Admim: Security

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth"
+import { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb"
+const adminEmails = process.env.ADMIN_EMAILS;
 
 export const authOptions = {
   providers: [
@@ -11,6 +13,23 @@ export const authOptions = {
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({session, token, user}) => {
+        if (adminEmails.includes(session?.user?.email)) {
+          return session;
+        } else {
+          return false;
+        }
+    } 
+  }
 }
 
 export default NextAuth(authOptions)
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  if(!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+  }
+}
